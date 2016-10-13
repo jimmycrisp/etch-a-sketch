b6c19abdc1b0201a78fad25abef324d5410593b6
James Crisp <Jimmyrcrisp@gmail.com>
2016-10-13 08:33:16 -0400

    Got to reset

--- jquery.js
+++ jquery.js
@@ -10,16 +10,24 @@ $(document).ready( function() {
 
   function addArea() {
     $('.container').append('<ul>', fart() ,  '</ul>')
-    };
+  };
   for (var i=0; i<11 ; i++) {
     addArea()
   };
+
 
   $('li').one('mouseover', function() {
     $(this).find('div').append('<div id="block2"></div>')
   });
 
 
+  $("#reset").click(function() {
+    document.location.reload(true);
+    var prompt = prompt("How many boxes?", "20");
+    if (prompt != null) {
+      document.getElementByID("demo").innerHTML =
+      "hello " + prompt + "! How are you today?";
+    }
+  });
 
-
-})
+});
