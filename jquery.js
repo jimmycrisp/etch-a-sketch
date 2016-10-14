b7b2208ac944a3c62252a35e36833026ca923ea9
James Crisp <Jimmyrcrisp@gmail.com>
2016-10-13 21:47:28 -0400

    final version

--- jquery.js
+++ jquery.js
@@ -2,23 +2,25 @@ $(document).ready( function() {
   var num2 = 1;
   var num1 = 1;
 
-  function ResizeTop(num2) {
+  function ResizeTop(num5) {
     function addTop() {
       $('#table').append('<tr class="block"></tr>')
     };
-    for (var i=0; i<(num2*10) ; i++) {
+    for (var i=0; i<(num5*10) ; i++) {
       addTop()
     };
   };
+  ResizeTop(num2);
 
-  function ResizeBottom(num2) {
+  function ResizeBottom(num4) {
     function addBottom() {
       $('tr').append('<td class="block"></td>');
     };
-    for (var j=0; j<(num2*10) ; j++) {
+    for (var j=0; j<(10*num4) ; j++) {
       addBottom()
     };
   };
+  ResizeBottom(num2);
 
   var safeColors = ['00','33','66','99','cc','ff'];
   var rand = function() {
@@ -31,20 +33,23 @@ $(document).ready( function() {
     return "#"+r+g+b;
   };
 
-  $('td').on('mouseover', function() {
-    $(this).css('background-color',randomColor())
-  });
+  function hover() {
+    $('td').on('mouseover', function() {
+      $(this).css('background-color',randomColor())
+    });
+  };
+  hover();
 
   $("#reset").click( function() {
     var value = prompt("Please enter grid size", "20");
-    $('.block').css('background-color', 'red');
+    $('#table').empty();
     var num = 500/value;
-    $('.block').css('height',num);
-    $('.block').css('width',num);
-    var num2 = value/10;
-    ResizeTop(num2);
-    ResizeBottom(num2);
-
+    var num3 = value/10;
+    ResizeTop(num3);
+    ResizeBottom(num3);
+    $('.block').css('height', num);
+    $('.block').css('width', num);
+    hover();
   });
 
 });
